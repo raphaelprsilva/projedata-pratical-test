760e3be93d71069add51c2030628ad166b2230da
Raphael Silva <raphaelprsilva@gmail.com>
2023-02-11 11:07:26 -0300

    first commit

---
+++ backend/src/main/java/com/projedata/backend/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.projedata.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Hello {
+
+  @GetMapping("/world")
+  public String hello() {
+    return "Hello!";
+  }
+}
